3997b508aff6bef33dfd0553de6f123b40127f8b
Asriel Camora <asriel@camora.dev>
2022-09-06 08:48:14 -0700

    Rewrite project declarations, platform support to cmake

--- CMakeL4Deps.cmake
+++ CMakeL4Deps.cmake
@@ -1,9 +1,23 @@
-function(L4_dependency_JSON VISIBILITY)
-    find_package(RapidJSON CONFIG REQUIRED)
-    target_link_libraries(${L4_CURRENT_PROJECT_SANITIZED_NAME} ${VISIBILITY} rapidjson)
+function(L4_dependency_JSON)
+    L4_add_dependency_findpkg(RapidJSON CONFIG LIBS rapidjson)
 endfunction()
 
-function(L4_dependency_HTTP VISIBILITY)
-    find_package(cpr CONFIG REQUIRED)
-    target_link_libraries(${L4_CURRENT_PROJECT_SANITIZED_NAME} ${VISIBILITY} cpr::cpr)
+function(L4_dependency_HTTP)
+    L4_add_dependency_findpkg(cpr CONFIG LIBS cpr::cpr)
+endfunction()
+
+function(L4_dependency_libbacktrace)
+    L4_add_dependency_manual(backtrace.h backtrace)
+endfunction()
+
+function(L4_dependency_breakpad)
+    L4_add_dependency_findpkg(unofficial-breakpad CONFIG LIBS unofficial::breakpad::libbreakpad unofficial::breakpad::libbreakpad_client)
+endfunction()
+
+function(L4_dependency_libfmt)
+    L4_add_dependency_findpkg(fmt CONFIG LIBS fmt::fmt)
+endfunction()
+
+function(L4_dependency_date)
+    L4_add_dependency_findpkg(date CONFIG LIBS date::date date::date-tz)
 endfunction()
